0ec8d45be7b67010fb3ca112835180df57b2dce2
20CS074Mandar <20CS074@charusat.edu.in>
2023-06-20 15:02:59 +0530

    Implemented the vedio integration with vimeo

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+$clientId = "1352a3ef754f70da7ea435548c2214b5c068c50d";
+$clientSecret = "EZ6Zb2I+TCtvAhf7y97S9p1YDrICj/rwh8K5lbcbJyUjm5Lbyh+Y/vflu+jPZIzEDlS9QEjaX8fmJu9XQjdhznjdd85xXVXgEjFYhTPTESFK7Xrw5JtofXZEAVupe4EJ";
+$accessToken = "87ed2c5906253f9442b59a500c2bc981";
